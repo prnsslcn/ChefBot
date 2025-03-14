cf4e326821701b800d40a1a1eead107efe93e41b
Choi Dongin <di0308@naver.com>
2025-03-14 16:36:28 +0900

    fix: api, render

--- frontend/chatbot-ui/src/components/AiChatValidate.js
+++ frontend/chatbot-ui/src/components/AiChatValidate.js
@@ -1,54 +1,26 @@
 import { useEffect, useMemo } from "react";
 import { useChat } from "../context/ChatContext";
 import { postQuery } from "../api/getQuery";
+import axios from "axios";
 
-export const AiChatBubble = () => {
+export const AiChatValidate = () => {
   const { aiResponse, callResponse, messages, setAiResponse, setCallResponse, setAllMessages, allMessages } = useChat();
 
   useEffect(() => {
     if (callResponse) {
-      
-      const latestMessage = messages[messages.length - 1];
-      setAiResponse((prevResponse) => [...prevResponse, latestMessage]);
-      setAllMessages((prevMessages) => [...prevMessages, {type: "ai", text: latestMessage}])
+      const fetchAiResponse = async () => {
+        try {
+          const response = await axios.get('http://localhost:5000/0');
+          setAiResponse((prevResponse) => [...prevResponse, response.data]);
+          setAllMessages((prevMessages) => [...prevMessages, response.data])
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      fetchAiResponse();
       setCallResponse(false);
-      
     }
   }, [callResponse]);
 
   return null;
 };
-
-// export const UserChatBubble = () => {
-//   const { messages } = useChat(); 
-//   const renderedMessages = useMemo(() => (
-//       messages.map((message, index) => (
-//       <div key={index} className="chat chat-end my-4">
-//           <div className="chat-bubble bg-sky-400 text-xl">
-//           {message}
-//           </div>
-//       </div>
-//       ))
-//   ), [messages]); 
-
-// return <div className="bg-white w-full p-4">{renderedMessages}</div>;
-// };
-
-// console.log(messages[messages.length - 1],aiResponse,callResponse)
-
-// useEffect(() => {
-//   if (callResponse) {
-//     const latestMessage = messages[messages.length - 1];
-//     const fetchAiResponse = async () => {
-//       try {
-//         const response = await postQuery(latestMessage);
-//         setAiResponse([response.data]);
-//         setCallResponse(false);
-//       } catch (error) {
-//         console.error('API 호출 실패:', error);
-//         setCallResponse(false);
-//       }
-//     };
-//     fetchAiResponse();
-//   }
-// }, [callResponse]);
--- frontend/chatbot-ui/src/context/ChatContext.js
+++ frontend/chatbot-ui/src/context/ChatContext.js
@@ -1,18 +1,19 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
     const [messages, setMessages] = useState([]);
-    const [aiResponse, setAiResponse] = useState(['어떤 요리를 만들고 싶으세요?']);
+    const [aiResponse, setAiResponse] = useState([]);
     const [callResponse, setCallResponse] = useState(false);
-    const [allMessages, setAllMessages] = useState([{
-        type: "ai", 
-        text: "어떤 요리를 만들고 싶으세요?'"
-    }])
+    const [allMessages, setAllMessages] = useState(['어떤 요리를 만들고 싶으세요?'])
 
     return (
-        <ChatContext.Provider value={{ messages, setMessages, aiResponse, setAiResponse, callResponse, setCallResponse, allMessages, setAllMessages }}>
+        <ChatContext.Provider value={{ 
+            messages, setMessages, 
+            aiResponse, setAiResponse, 
+            callResponse, setCallResponse, 
+            allMessages, setAllMessages }}>
             {children}
         </ChatContext.Provider>
     );
