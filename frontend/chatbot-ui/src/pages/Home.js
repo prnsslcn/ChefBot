9521173c7307090c5d8be61f97df0136360b7e5a
prnsslcnMac <prnsslcn@gmail.com>
2025-03-20 10:25:29 +0900

    Feat: 프론트엔드 디자인 개선

--- frontend/chatbot-ui/src/pages/Home.js
+++ frontend/chatbot-ui/src/pages/Home.js
@@ -4,14 +4,15 @@ import { UserInput } from "../components/UserInput";
 
 export default function Home() {
   return (
-    <div className="bg-stone-200 p-10 min-h-screen flex flex-col">
-      <AiChatValidate />
-      <AllChatBubble />
-      <UserInput />
+    <div className="w-full h-screen bg-white flex items-center justify-center">
+      <div className="w-[95%] max-w-[800px] h-[95%] bg-white rounded-2xl shadow-lg flex flex-col justify-between border border-neutral-200 overflow-hidden">
+        <div className="flex justify-center items-center p-4 border-b border-neutral-300">
+          <img src="https://img.icons8.com/stickers/100/cook-male.png" alt="Chatbot Logo" className="h-12" />
+        </div>
+        <AiChatValidate />
+        <AllChatBubble />
+        <UserInput />
+      </div>
     </div>
   );
 }
-
-/*
-2025-03-17-14:40 pull develop
-*/
