382add5e77c86edb3e752f75ea088c6c708527c5
KimSanghun <122427631+sanghunkim20@users.noreply.github.com>
2025-03-20 13:45:05 +0900

    Chore: 백엔드만 배포

--- frontend/chatbot-ui/src/components/LoadingSpinner.js
+++
@@ -1,9 +0,0 @@
-export const LoadingSpinner = () => {
-  return (
-    <div className="chat-message text-left mb-2 animate-fadein">
-      <div className="inline-block px-4 py-2 rounded-xl bg-neutral-100 text-black max-w-[70%] shadow text-base">
-        <div className="loading loading-dots loading-lg">...</div>
-      </div>
-    </div>
-  );
-};
